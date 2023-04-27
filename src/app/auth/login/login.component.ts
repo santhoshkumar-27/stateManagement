bedfcfbb19e4fa894570778c69459e7aa24753fe
santhoshkumar <its.santhoshkumar@outlook.com>
2023-04-27 07:05:42 +0530

    fix: login submit logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,9 +14,18 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.userInfor = this.fb.group({
-      userName: new FormControl(''),
-      passWord: new FormControl('')
+      userName: new FormControl('', [Validators.required]),
+      passWord: new FormControl('', [Validators.required])
     })
   }
-
+  get f() {
+    return this.userInfor.controls;
+  }
+  getControls(controllerName: string): FormControl {
+    return (this.userInfor.get(controllerName) as FormControl)
+  }
+  onLoginFormSubmit() {
+    const payload ={ ...this.userInfor.value}
+    console.log(payload);
+  }
 }
